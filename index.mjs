a0e59d387756a4e77d721677e98e72c76eb3462a
rodfaro <rodrigofaro2005@gmail.com>
2026-06-16 11:23:10 -0300

    Render y supabase

--- index.mjs
+++ index.mjs
@@ -1,11 +1,28 @@
+import 'dotenv/config'
 import express from 'express'
+import {Pool} from 'pg'
 
 const puerto = process.env.PORT || 4500
+
+//consulta
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: process.env.DB
+});
 
 const app = express()
 
 app.get('/test', (req, res)=>{
     res.send('funca')
+})
+
+app.get('/mensajes', async (req, res)=>{
+    const resultado = await pool.query('SELECT * FROM mensajes')
+    res.json(resultado.rows);
+
 })
 
 app.listen(puerto, ()=>{
